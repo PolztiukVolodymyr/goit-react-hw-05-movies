1cc06f2c3d2278cb69dd07a1db7c01963745e12b
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-08-02 01:35:22 +0300

    fix bug

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,6 +12,14 @@ const Cast = lazy(() => import('../Cast/Cast'));
 const Navigation = lazy(() => import('../Navigation/Navigation'));
 const Reviews = lazy(() => import('../Reviews/Reviews'));
 
+// import Home from "../../pages/HomePage";
+// import MovieDetailsPage from "../../pages/MovieDetailsPage";
+// import Movies from "../../pages/MoviesPage";
+
+// import Cast from "../../components/Cast/Cast";
+// import Navigation from "../../components/Navigation/Navigation";
+// import Reviews from "../../components/Reviews/Reviews";
+
 
 
 
