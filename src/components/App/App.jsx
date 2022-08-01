abbb57ab75502848125a1090c7a55e01b439f38a
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-08-01 11:33:45 +0300

    add Suspense and lazy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,35 @@
+
+import { Suspense, lazy } from "react";
 import { Route, Routes } from 'react-router-dom';
-
-import Home from "../../pages/HomePage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage";
-import Movies from "../../pages/MoviesPage";
-
-import Cast from "../Cast/Cast";
-import Navigation from "../Navigation/Navigation";
-import Reviews from "../Reviews/Reviews";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+const Home = lazy(() => import('../../pages/HomePage.jsx'));
+const MovieDetailsPage = lazy(() => import('../../pages/MovieDetailsPage.jsx'));
+const Movies = lazy(() => import('../../pages/MoviesPage'));
+
+const Cast = lazy(() => import('../Cast/Cast'));
+const Navigation = lazy(() => import('../Navigation/Navigation'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
+
+
+
 
 export const App = () => {
   return (
     <div>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-      </Routes>
-      <ToastContainer type="error" theme="colored" autoClose={3000}/>
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
+      <ToastContainer type="error" theme="colored" autoClose={3000} />
     </div>
   )
 };
